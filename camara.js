ef4ae444036a668896e461687c4f5e97275d3204
Julián R Figueroa <julianrfigueroa@gmail.com>
2013-10-26 19:06:47 -0500

    pasar prueba a jquery

---
+++ camara.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+
+      function takePhoto() {
+        //alert("takePhoto");
+        navigator.camera.getPicture(onCameraSuccess, onCameraError, {
+          quality : 50,
+          destinationType : Camera.DestinationType.FILE_URI
+        });
+        // navigator.camera.getPicture(onCameraSuccess, onCameraError);
+      }
+
+      function onCameraSuccess(imageURL) {
+        navigator.notification.alert("onCameraSuccess: " + imageURL);
+        //Get a handle to the image container div
+        ic = document.getElementById('imageContainer');
+        //Then write an image tag out to the div using the
+        //URL we received from the camera application. 
+        ic.innerHTML = '<img src="' + imageURL + '" width="100%" />';
+      }
+
+      function onCameraError(e) {
+        console.log(e);
+        navigator.notification.alert("onCameraError: " + e);
+      }

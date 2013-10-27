d7ec8f0267724596c6d150bd49852db9853ba6f1
Julián R Figueroa <julianrfigueroa@gmail.com>
2013-10-26 21:06:20 -0500

    listo para integrarse

--- camara.js
+++ camara.js
@@ -1,29 +1,26 @@
 // JavaScript Document
 
-      function takePhoto() {
-        //alert("takePhoto");
-        navigator.camera.getPicture(onCameraSuccess, onCameraError, {
-          quality : 50,
-          destinationType : Camera.DestinationType.FILE_URI
-        });
-        // navigator.camera.getPicture(onCameraSuccess, onCameraError);
-      }
 
-      function onCameraSuccess(imageURL) {
-        navigator.notification.alert("onCameraSuccess: " + imageURL);
-        //Get a handle to the image container div
-        ic = document.getElementById('imageContainer');
-        //Then write an image tag out to the div using the
-        //URL we received from the camera application. 
-        ic.innerHTML = '<img src="' + imageURL + '" width="100%" />';
-		msg = document.getElementById('mensaje');
-		msg.innerHTML = '';
-		phBtn = document.getElementById('photoBtn');
-		phBtn.value = 'Retomar foto';
-		
-      }
+function takePhoto() {
+  navigator.camera.getPicture(onCameraSuccess, onCameraError, {
+    quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI
+  });
+}
 
-      function onCameraError(e) {
-        console.log(e);
-        navigator.notification.alert("onCameraError: " + e);
-      }
+function onCameraSuccess(imageURL) {
+  //Get a handle to the image container div
+  ic = document.getElementById('imageContainer');
+  //Then write an image tag out to the div using the
+  //URL we received from the camera application. 
+  ic.innerHTML = '<img src="' + imageURL + '" width="100%" />';
+  msg = document.getElementById('mensaje');
+  msg.innerHTML = '';
+  document.getElementById('photoBtn').value = 'Retomar foto';
+  $("#photoBtn").button('refresh');
+}
+
+function onCameraError(e) {
+  console.log(e);
+  navigator.notification.alert("onCameraError: " + e);
+}

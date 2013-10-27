e226013832c0fb933f87f06fbed5ac70d3848f7b
Julián R Figueroa <julianrfigueroa@gmail.com>
2013-10-27 01:39:04 -0500

    imageData en canvas, para el parse a base64

--- camara.js
+++ camara.js
@@ -1,10 +1,10 @@
 // JavaScript Document
 
 var licenseBool = false
-var type = "no_type"
-var comment = "no_comment"
+var type = 'no_type'
+var comment = 'no_comment'
 var imageData = 'no_image'
-var urlForm = "http://www.aktio.co/eltopo/register.php"
+var urlForm = 'http://www.aktio.co/eltopo/register.php'
 
   function takePhoto() {
     navigator.camera.getPicture(onCameraSuccess, onCameraError, {
@@ -15,54 +15,58 @@ var urlForm = "http://www.aktio.co/eltopo/register.php"
 
   function onCameraSuccess(imageURI) {
     console.log(imageURI)
-    ic = document.getElementById('imageContainer')
+    ic = document.getElementById('image')
     ic.src = imageURI
 
-    var c = document.createElement("canvas")
-    var ctx = c.getContext("2d")
-    var img = document.getElementById("imageContainer")
-    ctx.drawImage(img, 10, 10)
+    var c = document.getElementById('canvas')
+    c.height = ic.clientHeight
+    c.width = ic.clientWidth
+    var ctx = c.getContext('2d')
+    ctx.drawImage(ic, 0, 0)
     imageData = c.toDataURL()
+
+    ic.src = imageData
+
     console.log(imageData)
 
     document.getElementById('photoBtn').value = 'Retomar foto'
-    $("#photoBtn").button('refresh')
+    $('#photoBtn').button('refresh')
   }
 
   function onCameraError(e) {
     console.log(e)
-    navigator.notification.alert("onCameraError: " + e)
+    navigator.notification.alert('onCameraError: ' + e)
   }
 
   function sendInfo() {
-    if ($("#license").is(":checked")) {
+    if ($('#license').is(':checked')) {
       licenseBool = true
     } else {
       licenseBool = false
     }
-    if ($("#homeType").is(":checked")) {
-      type = "home"
-    } else if ($("#buildingType").is(":checked")) {
-      type = "building"
-    } else if ($("#mallType").is(":checked")) {
-      type = "mall"
+    if ($('#homeType').is(':checked')) {
+      type = 'home'
+    } else if ($('#buildingType').is(':checked')) {
+      type = 'building'
+    } else if ($('#mallType').is(':checked')) {
+      type = 'mall'
     } else {
-      type = "no_type"
+      type = 'no_type'
     }
     var toSend = {
-      "image": imageData,
-      "position": {
-        "latitude": document.getElementById('latitude').value,
-        "longitude": document.getElementById('longitude').value
+      'image': imageData,
+      'position': {
+        'latitude': document.getElementById('latitude').value,
+        'longitude': document.getElementById('longitude').value
       },
-      "license": licenseBool,
-      "type": type,
-      "comment": document.getElementById('comment').value
+      'license': licenseBool,
+      'type': type,
+      'comment': document.getElementById('comment').value
     }
     console.log(toSend)
 
     $.ajax({
-      type: "POST",
+      type: 'POST',
       url: urlForm,
       data: toSend
     }).done(function(msg) {

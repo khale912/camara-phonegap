034ad0a1d437f243f46759f9356b2446b0d1a53b
Julián R Figueroa <julianrfigueroa@gmail.com>
2013-10-27 12:45:08 -0500

    integracion con animacion

--- js/camara.js
+++ js/camara.js
@@ -1,8 +1,10 @@
 // JavaScript Document
 
   function takePhoto() {
+
     // Este es para probar en local
     //onCameraSuccess("images/test.jpeg");
+
     navigator.camera.getPicture(onCameraSuccess, onCameraError, {
       quality: 10,
       destinationType: Camera.DestinationType.FILE_URI
@@ -15,7 +17,7 @@
     ic.src = imageURI
 
     var divBtnGoToForm = document.getElementById('btnGoToForm')
-    divBtnGoToForm.innerHTML =  '<a href="#form" id="goToFormBtn" data-role="button">Lista la foto</a>'
+    divBtnGoToForm.innerHTML =  '<a href="#form" id="goToFormBtn" data-role="button" >Lista la foto</a>'
     $('#goToFormBtn').button().button('refresh')
 
     document.getElementById('photoBtn').value = 'Retomar foto'
@@ -27,7 +29,7 @@
     c.height = ic.naturalHeight
     c.width = ic.naturalWidth
     var ctx = c.getContext('2d')
-    ctx.drawImage(ic, 0, 0)
+    ctx.drawImage(ic, 0, 0, 375, 529)
     imageData = c.toDataURL()
 
     console.log(imageData)
